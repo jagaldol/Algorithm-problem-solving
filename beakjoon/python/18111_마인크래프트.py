5335cc7e65b44b7bd7660538db56b7134de10cb5
jagaldol <yoy06056@pusan.ac.kr>
2023-11-14 03:25:29 +0900

    feat: 18111 알고리즘 개선

--- beakjoon/python/18111_마인크래프트.py
+++ beakjoon/python/18111_마인크래프트.py
@@ -1,42 +1,36 @@
 def input_process():
     N, M, B = map(int, input().split())
 
-    heights = []
+    counts = [0] * 257
 
     for i in range(N):
         row = list(map(int, input().split()))
-        heights.append(row)
-
-    return N, M, B, heights
-
-
-def calculate_time(N, M, B, heights, target):
-    time = 0
-
-    for i in range(N):
         for j in range(M):
-            gap = target - heights[i][j]
-            B -= gap
-            time += gap if gap > 0 else -2 * gap
-    
-    return time, B
+            counts[row[j]] += 1
+
+    return N, M, B, counts
 
 
-if __name__ == "__main__":
-    N, M, B, heights = input_process()
-
-    min_height, max_height = min(map(min, heights)), max(map(max, heights))
+def sol():
+    N, M, B, counts = input_process()
 
     min_time = 200000000
     max_target = 0
-    for target in range(min_height, max_height + 1):
-        time, remain_block = calculate_time(N, M, B, heights, target)
-        if remain_block < 0:
-            continue
-        if time < min_time:
-            min_time = time
-            max_target = target
-        elif time == min_time and max_target < target:
-            max_target = target
+
+    for target in range(257):
+        low = high = 0
+        for i in range(0, 257):
+            cost = abs(target - i) * counts[i]
+            if (i < target):
+                low += cost
+            else:
+                high += cost
+
+        if high + B >= low:
+            time = high * 2 + low
+            if time <= min_time:
+                min_time, max_target = time, target
 
     print(min_time, max_target)
+
+sol()
