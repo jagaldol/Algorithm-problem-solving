e794fda0bcc41f53d6efff1823277905eb7179a1
jagaldol <yoy06056@pusan.ac.kr>
2024-03-21 21:35:47 +0900

    정수 삼각형 level 3 해결

---
+++ programmers/level_3/정수 삼각형.py
@@ -0,0 +1,13 @@
+def solution(triangle):
+    dp = [row[:] for row in triangle]
+
+    for i in range(1, len(triangle)):
+        for j in range(i + 1):
+            if j == 0:
+                dp[i][j] += dp[i - 1][j]
+            elif j == i:
+                dp[i][j] += dp[i - 1][j - 1]
+            else:
+                dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j])
+
+    return max(dp[len(triangle) - 1])
