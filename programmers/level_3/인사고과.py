e440b2740033c122c7ad7eb178c841d632f18b08
jagaldol <yoy06056@pusan.ac.kr>
2024-03-23 00:40:46 +0900

    인사고과 level 3 해결

---
+++ programmers/level_3/인사고과.py
@@ -0,0 +1,23 @@
+def solution(scores):
+    failed = [False for _ in scores]
+    scores = [(idx, score) for idx, score in enumerate(scores)]
+
+    sorted_scores = sorted(scores, key=lambda x: (-x[1][0], x[1][1]))
+
+    max_value = sorted_scores[0][1][1]
+
+    for idx, score in sorted_scores:
+        if score[1] < max_value:
+            failed[idx] = True
+        else:
+            max_value = score[1]
+
+    if failed[0]:
+        return -1
+
+    my_score = sum(scores[0][1])
+
+    return (
+        sum([1 for idx, score in scores if not failed[idx] and sum(score) > my_score])
+        + 1
+    )
