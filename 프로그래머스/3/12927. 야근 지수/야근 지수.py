50deff98e89ba2952501f4a972c1144ec96a6508
안혜준 <jagaldol.dev@gmail.com>
2024-05-27 17:45:37 +0900

    [level 3] Title: 야근 지수, Time: 439.85 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,15 +1,12 @@
 import heapq
 
 def solution(n, works):
-    answer = 0
-    pq = [-work for work in works]
-    heapq.heapify(pq)
+    heapq.heapify(pq := [-work for work in works])
+    
     for i in range(n):
         if len(pq) == 0:
             return 0
-        current = heapq.heappop(pq)
-        if current < 0:
+        if (current := heapq.heappop(pq)) < 0:
             heapq.heappush(pq, current + 1)
-    
     
     return sum([work**2 for work in pq])
