727875d9b69a9b840b657a88144bd33592451b5b
안혜준 <jagaldol.dev@gmail.com>
2024-06-07 14:20:56 +0900

    [Gold IV] Title: 테트로미노, Time: 252 ms, Memory: 114916 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -6,23 +6,26 @@ N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 visited = [[False] * M for _ in range(N)]
 answer = 0
+mx = max(one for row in board for one in row)
 
 steps = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
 
 def dfs(path):
+    sum_value = sum(board[i][j] for i, j in path)
     if len(path) == 4:
         global answer
-        answer = max(answer, sum(board[i][j] for i, j in path))
+        answer = max(answer, sum_value)
         return
+    if sum_value + (4 - len(path)) * mx <= answer:
+        return
+
     for x, y in path:
         for tx, ty in steps:
             nx, ny = x + tx, y + ty
             if nx in range(N) and ny in range(M) and not visited[nx][ny]:
                 visited[nx][ny] = True
-                path.append((nx, ny))
-                dfs(path)
-                path.pop()
+                dfs(path + [(nx, ny)])
                 visited[nx][ny] = False
 
 
