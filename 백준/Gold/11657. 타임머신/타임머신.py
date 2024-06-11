15ecc47a455f2dcc15dc369a2ab7fd25667c7e72
안혜준 <jagaldol.dev@gmail.com>
2024-06-11 16:54:09 +0900

    [Gold IV] Title: 타임머신, Time: 208 ms, Memory: 111004 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -0,0 +1,30 @@
+import sys
+
+INF = 1_000_000_000
+
+
+def sol():
+    N, M = map(int, input().split())
+
+    buses = []
+    for _ in range(M):
+        A, B, C = map(int, input().split())
+        buses.append((A, B, C))
+
+    dist = [INF if i != 1 else 0 for i in range(N + 1)]
+
+    for i in range(N):
+        for A, B, C in buses:
+            if dist[A] != INF and dist[B] > dist[A] + C:
+                dist[B] = dist[A] + C
+
+    for A, B, C in buses:
+        if dist[A] != INF and dist[B] > dist[A] + C:
+            print(-1)
+            return
+
+    for i in range(2, N + 1):
+        print(dist[i] if dist[i] != INF else -1)
+
+
+sol()
