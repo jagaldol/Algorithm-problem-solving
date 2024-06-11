e7c21cd1b301bc0d14b92fbdf4ee8f080413759f
안혜준 <jagaldol.dev@gmail.com>
2024-06-11 16:47:30 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int result = 0;
+
+	for (int i = 0; i < 5; i++) {
+		cin >> n;
+		result += n * n;
+	}
+	cout << result % 10 << '\n';
+
+	return 0;
+}
