c36a0393faf181bd94cf4846988540160034aaf0
안혜준 <jagaldol.dev@gmail.com>
2024-06-11 17:45:14 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -0,0 +1,85 @@
+arounds = [
+    [-2, 0],
+    [-1, -1],
+    [0, -2],
+    [1, -1],
+    [2, 0],
+    [1, 1],
+    [0, 2],
+    [-1, 1]
+]
+
+arounds_allow = [
+    [[-1, 0]],
+    [[0, -1], [-1, 0]],
+    [[0, -1]],
+    [[0, -1], [1, 0]],
+    [[1, 0]],
+    [[0, 1], [1, 0]],
+    [[0, 1]],
+    [[0, 1], [-1, 0]]
+]
+
+nears = [
+    [-1, 0],
+    [0, -1],
+    [1, 0],
+    [0, 1]
+]
+
+
+def solution(places):
+    answer = []
+    for place in places:
+        print(place)
+        if isBad(place):
+            answer.append(0)
+        else:
+            answer.append(1)
+    return answer
+
+
+def isBad(place):
+    for i in range(5):
+        for j in range(5):
+            now = place[i][j]
+            if now == 'P':
+                if HasNear(place, i, j):
+                    return True
+    return False
+
+                
+def HasNear(place, i, j):
+    for near in nears:
+        x = i + near[0]
+        y = j + near[1]
+        if not (0 <= x < 5) or not (0 <= y < 5):
+            continue
+        if place[x][y] == 'P':
+            return True
+
+    for idx in range(len(arounds)):
+        x = i + arounds[idx][0]
+        y = j + arounds[idx][1]
+        if not (0 <= x < 5) or not (0 <= y < 5):
+            continue
+        if place[x][y] == 'P':
+            if not exist_partition(place, i, j, idx):
+                return True
+    return False
+        
+
+def exist_partition(place, i, j, idx):
+    check_list = arounds_allow[idx]
+    
+    print(i, j, idx)
+    for offset in check_list:
+        print(place[i + offset[0]][j + offset[1]])
+        if place[i + offset[0]][j + offset[1]] != 'X':
+            return False
+    print("파티션 존재")
+    return True
+    
+    
+    
+    
