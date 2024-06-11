0ef24c698240a1cd0ac91313889ab8e77cd06aa8
jagaldol <yoy06056@pusan.ac.kr>
2024-06-11 18:05:57 +0900

    refactor: 백준 허브 양식에 맞게 폴더 구조 변경
    프로그래머스 적용

--- programmers/level_1/과일 장수.py
+++
@@ -1,7 +0,0 @@
-def solution(k, m, score):
-    sorted_score = sorted(score, reverse=True)
-    answer = 0
-    for i in range(m - 1, len(sorted_score), m):
-        answer += sorted_score[i] * m
-
-    return answer
