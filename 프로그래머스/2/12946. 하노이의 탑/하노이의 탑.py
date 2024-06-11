3b9890ed3d4e282cf6c018d814bbe678b665c5f9
안혜준 <jagaldol.dev@gmail.com>
2024-06-11 17:47:36 +0900

    [level 2] Title: 하노이의 탑, Time: 16.68 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,15 @@
+answer = []
+
+def hanoi(n, s, e):
+    if n == 1:
+        answer.append([s, e])
+        return
+    empty_place = [i for i in [1, 2, 3] if not i in [s, e]][0]
+    hanoi(n - 1, s, empty_place)
+    answer.append([s, e])
+    hanoi(n - 1, empty_place, e)
+    
+
+def solution(n):
+    hanoi(n, 1, 3)
+    return answer
