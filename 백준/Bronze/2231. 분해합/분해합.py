850c62cf58adf6296dc38536bf61c5323f8e03aa
Hyejun An <jagaldol.dev@gmail.com>
2024-12-28 20:38:03 +0900

    [Bronze II] Title: 분해합, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -2,7 +2,7 @@ N = input()
 
 
 def sol():
-    for i in range(1, int(N)):
+    for i in range(max(1, int(N) - 9 * len(N)), int(N)):
         target = i + sum(map(int, str(i)))
         if target == int(N):
             print(i)
