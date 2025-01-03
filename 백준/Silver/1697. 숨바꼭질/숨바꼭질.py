fe1fad454614e1b1c5e7e37585521dc9c167a49f
Hyejun An <jagaldol.dev@gmail.com>
2025-01-03 18:51:57 +0900

    [Silver I] Title: 숨바꼭질, Time: 224 ms, Memory: 153228 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+
+def sol():
+    queue = deque([(N, 0)])
+    visited = [False for _ in range(100_001)]
+    while queue:
+        x, t = queue.popleft()
+        if x == K:
+            print(t)
+            break
+        visited[x] = True
+        nxs = [2 * x, x + 1, x - 1]
+        for nx in nxs:
+            if nx in range(100_001) and not visited[nx]:
+                queue.append((nx, t + 1))
+
+
+sol()
