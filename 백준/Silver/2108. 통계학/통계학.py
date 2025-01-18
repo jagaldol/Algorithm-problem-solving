96262b45219050c8a3c17199f0fb80884b19ac7a
Hyejun An <jagaldol.dev@gmail.com>
2025-01-18 23:22:21 +0900

    [Silver III] Title: 통계학, Time: 284 ms, Memory: 120500 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,26 @@
+import sys
+from collections import Counter
+
+input = sys.stdin.readline
+N = int(input())
+numbers = [int(input()) for _ in range(N)]
+
+
+def round(num):
+    return int(num + 0.5) if num > 0 else int(num - 0.5)
+
+
+def sol():
+    numbers.sort()
+    print(round(sum(numbers) / len(numbers)))
+    print(numbers[len(numbers) // 2])
+    counter = Counter(numbers)
+    mode = counter.most_common()
+    if len(numbers) > 1 and mode[0][1] == mode[1][1]:
+        print(mode[1][0])
+    else:
+        print(mode[0][0])
+    print(numbers[-1] - numbers[0])
+
+
+sol()
