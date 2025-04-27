b1cce4b9b6799e059871d6fbb64796af94add14a
Hyejun An <jagaldol.dev@gmail.com>
2025-04-27 17:10:39 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 7344 ms, Memory: 668140 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -1,44 +1,35 @@
 import sys
-from itertools import product
 
 input = sys.stdin.readline
 n = int(input())
-A = []
-B = []
-C = []
-D = []
-for _ in range(n):
-    a, b, c, d = map(int, input().split())
-    A.append(a)
-    B.append(b)
-    C.append(c)
-    D.append(d)
+A, B, C, D = [0] * n, [0] * n, [0] * n, [0] * n
+for i in range(n):
+    A[i], B[i], C[i], D[i] = map(int, input().split())
 
 
 def sol():
-    AB = sorted(a + b for a, b in product(A, B))
-    CD = sorted(c + d for c, d in product(C, D))
+    AB = sorted(a + b for a in A for b in B)
+    CD = sorted(c + d for c in C for d in D)
+    length = len(AB)
 
     answer = 0
 
     left = 0
-    right = len(AB) - 1
-    while left < len(AB) and right >= 0:
+    right = length - 1
+    while left < length and right >= 0:
         total_sum = AB[left] + CD[right]
         if total_sum == 0:
             ab_count = 1
             cd_count = 1
-            while left < len(AB) - 1 and AB[left] == AB[left + 1]:
+            while left < length - 1 and AB[left] == AB[left + 1]:
                 left += 1
                 ab_count += 1
             while right > 0 and CD[right] == CD[right - 1]:
                 right -= 1
                 cd_count += 1
             answer += ab_count * cd_count
-            if right > 0:
-                right -= 1
-            else:
-                left += 1
+            left += 1
+            right -= 1
         elif total_sum < 0:
             left += 1
         else:
