636767c0474d33b7ffb52600500d5a5b67c79ecc
Hyejun An <jagaldol.dev@gmail.com>
2025-04-29 18:14:43 +0900

    [Gold I] Title: 계단 수, Time: 132 ms, Memory: 114628 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.py
+++ 백준/Gold/1562. 계단 수/계단 수.py
@@ -3,16 +3,16 @@ N = int(input())
 dp = [[[0 for _ in range(1024)] for _ in range(10)] for _ in range(N)]
 
 for j in range(1, 10):
-    dp[0][j][2**j] = 1
+    dp[0][j][1 << j] = 1
 
 mod = 1_000_000_000
 for i in range(1, N):
     for j in range(10):
         for mask in range(1024):
             if j > 0:
-                dp[i][j][mask | 2**j] += dp[i - 1][j - 1][mask]
+                dp[i][j][mask | 1 << j] += dp[i - 1][j - 1][mask]
             if j < 9:
-                dp[i][j][mask | 2**j] += dp[i - 1][j + 1][mask]
-            dp[i][j][mask | 2**j] %= mod
+                dp[i][j][mask | 1 << j] += dp[i - 1][j + 1][mask]
+            dp[i][j][mask | 1 << j] %= mod
 
 print(sum(dp[N - 1][j][1023] for j in range(10)) % mod)
