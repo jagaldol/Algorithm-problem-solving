d7810967af8840bbff3ce57f2d731e812ba34f00
Hyejun An <jagaldol.dev@gmail.com>
2025-04-29 18:15:29 +0900

    [Gold I] Title: 계단 수, Time: 128 ms, Memory: 114628 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.py
+++ 백준/Gold/1562. 계단 수/계단 수.py
@@ -9,10 +9,11 @@ mod = 1_000_000_000
 for i in range(1, N):
     for j in range(10):
         for mask in range(1024):
+            n_mask = mask | 1 << j
             if j > 0:
-                dp[i][j][mask | 1 << j] += dp[i - 1][j - 1][mask]
+                dp[i][j][n_mask] += dp[i - 1][j - 1][mask]
             if j < 9:
-                dp[i][j][mask | 1 << j] += dp[i - 1][j + 1][mask]
-            dp[i][j][mask | 1 << j] %= mod
+                dp[i][j][n_mask] += dp[i - 1][j + 1][mask]
+            dp[i][j][n_mask] %= mod
 
 print(sum(dp[N - 1][j][1023] for j in range(10)) % mod)
