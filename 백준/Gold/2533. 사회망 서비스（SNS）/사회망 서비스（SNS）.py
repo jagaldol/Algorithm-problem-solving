0d97651d96fad23f7ac0c13278c98188b74096ac
Hyejun An <jagaldol.dev@gmail.com>
2025-05-06 21:41:37 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 4900 ms, Memory: 300588 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -1,30 +1,36 @@
 import sys
 
 sys.setrecursionlimit(10**6)
-
 input = sys.stdin.readline
+
 N = int(input())
 graph = [[] for _ in range(N + 1)]
+dp = [[0, 1] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+parent = [0] * (N + 1)
+
 for _ in range(N - 1):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
-visited = [0] * (N + 1)
-dp = [[0, 1] for _ in range(N + 1)]
+stack = [(1, False)]  # (현재 노드, 자식 처리가 끝났는지 여부)
 
+while stack:
+    node, processed = stack.pop()
+    if not processed:
+        visited[node] = True
+        stack.append((node, True))  # 나중에 다시 처리
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                parent[next_node] = node
+                stack.append((next_node, False))
+    else:
+        # 자식 정보가 다 채워졌으니 dp 갱신
+        for child in graph[node]:
+            if child == parent[node]:
+                continue
+            dp[node][0] += dp[child][1]
+            dp[node][1] += min(dp[child][0], dp[child][1])
 
-def recursive(node):
-    if visited[node]:
-        return
-    visited[node] = 1
-    for node2 in graph[node]:
-        if visited[node2]:
-            continue
-        recursive(node2)
-        dp[node][0] += dp[node2][1]
-        dp[node][1] += min(dp[node2][0], dp[node2][1])
-
-
-recursive(1)
 print(min(dp[1][0], dp[1][1]))
