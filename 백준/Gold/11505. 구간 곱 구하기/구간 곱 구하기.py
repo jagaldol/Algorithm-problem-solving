841f19d29f9368ab6ec9d3c9eb75c89c386d3523
Hyejun An <jagaldol.dev@gmail.com>
2025-05-09 13:56:42 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 484 ms, Memory: 161976 KB -BaekjoonHub

---
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
@@ -0,0 +1,50 @@
+import sys
+
+input = sys.stdin.readline
+N, M, K = map(int, input().split())
+A = [0] + [int(input()) for _ in range(N)]
+
+tree = [0] * (4 * N)
+MOD = 10**9 + 7
+
+
+def build(node, start, end):
+    if start == end:
+        tree[node] = A[start]
+    else:
+        mid = (start + end) // 2
+        build(node * 2, start, mid)
+        build(node * 2 + 1, mid + 1, end)
+        tree[node] = tree[node * 2] * tree[node * 2 + 1] % MOD
+
+
+def update(node, start, end, idx, value):
+    if start == end:
+        A[idx] = value
+        tree[node] = value
+    else:
+        mid = (start + end) // 2
+        if idx <= mid:
+            update(node * 2, start, mid, idx, value)
+        else:
+            update(node * 2 + 1, mid + 1, end, idx, value)
+        tree[node] = tree[node * 2] * tree[node * 2 + 1] % MOD
+
+
+def query(node, start, end, left, right):
+    if right < start or end < left:
+        return 1
+    if left <= start and end <= right:
+        return tree[node]
+    mid = (start + end) // 2
+    return query(node * 2, start, mid, left, right) * query(node * 2 + 1, mid + 1, end, left, right) % MOD
+
+
+build(1, 1, N)
+
+for _ in range(M + K):
+    a, b, c = map(int, input().split())
+    if a == 1:
+        update(1, 1, N, b, c)
+    else:
+        print(query(1, 1, N, b, c))
